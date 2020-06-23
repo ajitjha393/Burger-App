720b400fc38242f4d29b582e840e8f5f36cff25c
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-23 19:50:48 +0530

    Fixed state of Total Price Calculation

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,12 @@
 import * as actionTypes from './actions'
 
+const INGREDIENT_PRICES = {
+	salad: 0.5,
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7,
+}
+
 const initialState = {
 	ingredients: {
 		salad: 0,
@@ -20,10 +27,12 @@ const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.ADD_INGREDIENT:
 			newState.ingredients[action.ingredientType] += 1
+			newState.totalPrice += INGREDIENT_PRICES[action.ingredientType]
 			break
 
 		case actionTypes.REMOVE_INGREDIENT:
 			newState.ingredients[action.ingredientType] -= 1
+			newState.totalPrice -= INGREDIENT_PRICES[action.ingredientType]
 			break
 
 		default:
