96d66d980f8abb566c5da93594dcef7266ab0c54
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-23 18:08:14 +0530

    Implemented Reducer to Add/remove ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,35 @@
 import * as actionTypes from './actions'
 
 const initialState = {
-	ingredients: null,
+	ingredients: {
+		salad: 0,
+		bacon: 0,
+		cheese: 0,
+		meat: 0,
+	},
 	totalPrice: 4,
 }
 
 const reducer = (state = initialState, action) => {
-	return state
+	const newState = {
+		...state,
+		ingredients: {
+			...state.ingredients,
+		},
+	}
+	switch (action.type) {
+		case actionTypes.ADD_INGREDIENT:
+			newState.ingredients[action.ingredientType] += 1
+			break
+
+		case actionTypes.REMOVE_INGREDIENT:
+			newState.ingredients[action.ingredientType] -= 1
+			break
+
+		default:
+			break
+	}
+	return newState
 }
 
 export default reducer
