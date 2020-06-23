4b61ec4d64566c4c3ab14d36369b001ab2246dc7
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-23 22:58:39 +0530

    Handled props data using redux store

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,8 @@ import Input from '../../../components/UI/Input/Input'
 
 import classes from './ContactData.module.css'
 
+import { connect } from 'react-redux'
+
 class ContactData extends Component {
 	state = {
 		orderForm: {
@@ -234,4 +236,11 @@ class ContactData extends Component {
 	}
 }
 
-export default ContactData
+const mapStateToProps = (state) => {
+	return {
+		ingredients: state.ingredients,
+		price: state.totalPrice,
+	}
+}
+
+export default connect(mapStateToProps)(ContactData)
