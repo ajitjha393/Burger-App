d9bfbac5cc42f4173e6edc62dba0dbee6799fbd3
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-25 16:13:45 +0530

    Added initIngredients for dispatching in async way

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,5 +1,7 @@
 import * as actionTypes from './actionTypes'
 
+import axios from '../../axios-orders'
+
 export const addIngredient = (ingredientType) => {
 	return {
 		type: actionTypes.ADD_INGREDIENT,
@@ -11,5 +13,22 @@ export const removeIngredient = (ingredientType) => {
 	return {
 		type: actionTypes.REMOVE_INGREDIENT,
 		ingredientType,
+	}
+}
+
+const setIngredients = (ingredients) => {
+	return {
+		type: actionTypes.SET_INGREDIENT,
+		ingredients,
+	}
+}
+
+export const initIngredients = () => {
+	return async (dispatch) => {
+		const response = await axios.get(
+			'https://react-burger-app-11993.firebaseio.com/ingredients.json'
+		)
+		const ingredients = response.data
+		return dispatch(setIngredients(ingredients))
 	}
 }
