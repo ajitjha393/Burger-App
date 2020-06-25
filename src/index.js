8972a003c8daa612f309d64933779c2fce38341d
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-25 15:38:34 +0530

    Fixed the imports for reducer

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
 import { createStore } from 'redux'
-import reducer from './store/reducers/burgerBuilder'
+import burgerBuilderReducer from './store/reducers/burgerBuilder'
 import { Provider } from 'react-redux'
 
 const store = createStore(
-	reducer,
+	burgerBuilderReducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
