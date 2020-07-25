7763bbec09e286249faa2a8c89b56bc0f71a232e
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-25 17:10:22 +0530

    Requested a token from firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,9 @@
 import * as actionTypes from './actionTypes'
 
+import axios from 'axios'
+
+const API_KEY = 'AIzaSyAIqoQ8VwUy-X2shAGYNL0vdArYQii6v9g'
+
 export const authStart = () => {
 	return {
 		type: actionTypes.AUTH_START,
@@ -21,7 +25,24 @@ export const authFail = (error) => {
 }
 
 export const auth = (email, password) => {
-	return (dispatch) => {
+	return async (dispatch) => {
 		dispatch(authStart())
+
+		try {
+			const res = await axios.post(
+				`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+				{
+					email,
+					password,
+					returnSecureToken: true,
+				}
+			)
+			console.log(res)
+
+			dispatch(authSuccess(res.data))
+		} catch (err) {
+			console.log(err)
+			dispatch(authFail(err))
+		}
 	}
 }
