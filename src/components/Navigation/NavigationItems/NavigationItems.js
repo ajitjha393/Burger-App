d658f942aa909c3329f4de5a1c3ac89b97369e64
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-26 17:18:06 +0530

    Conditionally render Auth / logout routes

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,12 @@ const navigationItems = (props) => (
 		<NavigationItem link={'/'}>Burger Builder</NavigationItem>
 
 		<NavigationItem link={'/orders'}>Orders</NavigationItem>
-		<NavigationItem link={'/auth'}>Authenticate</NavigationItem>
+
+		{!props.isAuthenticated ? (
+			<NavigationItem link={'/auth'}>Authenticate</NavigationItem>
+		) : (
+			<NavigationItem link={'/logout'}>Logout</NavigationItem>
+		)}
 	</ul>
 )
 
