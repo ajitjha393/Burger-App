814de33aa6c17afc3c98d3841a29a5afdceb5891
Bishwajit Jha <ajitjha393@gmail.com>
2020-08-06 10:02:11 +0530

    Fixed Redux enhancers

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
 const composeEnhancers =
-	process.env.NODE_ENV === 'development'
+	(process.env.NODE_ENV === 'development'
 		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-		: null || compose
+		: null) || compose
 
 const rootReducer = combineReducers({
 	burgerBuilder: burgerBuilderReducer,
